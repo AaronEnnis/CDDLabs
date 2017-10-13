9877f65f552186fc659bc489cd8a9924e7364447
AaronEnnis <aaronennis96@outlook.com>
2017-10-13 14:29:48 +0100

    Lab4 third commit

--- Lab4/barrier.cpp
+++ Lab4/barrier.cpp
@@ -3,32 +3,58 @@
 #include <thread>
 
 int count = 0;
-bool condition = true;
 
-void taskOne(std::shared_ptr<Semaphore> barrier1, std::shared_ptr<Semaphore> barrier2){
-  theSemaphore->Wait();
-  std::cout << "number " << count << " logged in\n";
-  count++;
-  theSemaphore->Signal();
+void taskOne(){
+  std::cout << "first " << count << "\n";
 }
 
-void taskTwo(std::shared_ptr<Semaphore> barrier1, std::shared_ptr<Semaphore> barrier2){
-  theSemaphore->Wait();
-  std::cout << "number " << count << " logged in\n";
-  count--;
-  theSemaphore->Signal();
+void taskTwo(){
+  std::cout << "second " << count << "\n";
+}
+
+void barrierFunction(std::shared_ptr<Semaphore> mutex,std::shared_ptr<Semaphore> barrier1, std::shared_ptr<Semaphore> barrier2){
+
+    bool condition = true;
+  
+    while(condition){
+    mutex->Wait();
+    count++;
+    if(count == threadArray.size() - 1){
+      barrier2->Wait();
+      barrier1->Signal();      
+    }
+    mutex->Signal();
+    barrier1->Wait();
+    barrier1->Signal();
+
+    threadArray[count]=std::thread(taskOne);
+
+    mutex->Wait();
+    count--;
+    if(count == 0){
+      barrier1->Wait();
+      barrier2->Signal();
+      condition = false;
+    }
+    mutex->Signal();
+    barrier2->Wait();
+    barrier2->Signal();
+
+    threadArray[count]=std::thread(taskTwo);
+  }
+
+
 }
 
 int main(void){
-  std::thread threadArray[10];
+  std::array<std::thread,10> threadArray;
   std::shared_ptr<Semaphore> mutex( new Semaphore(1));
   std::shared_ptr<Semaphore> barrier1( new Semaphore(0));
   std::shared_ptr<Semaphore> barrier2( new Semaphore(1));
   /**< Launch the threads  */
-  while(condition){
-    threadArray=std::thread(taskOne,mutex,barrier1,barrier2);
 
-    threadArray=std::thread(taskTwo,mutex,barrier1,barrier2);
+  for(int i = 0; i < threadArray.size();i++){
+  threadArray[i]=std::thread(barrierFunction, mutex, barrier1, barrier2);
   }
 
   return 0;
