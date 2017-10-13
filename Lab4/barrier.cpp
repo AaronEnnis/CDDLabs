8f3dad88e65ec25fb6c3ba90398e3125b2ee5860
AaronEnnis <aaronennis96@outlook.com>
2017-10-13 14:49:39 +0100

    Lab4 final commit

--- Lab4/barrier.cpp
+++ Lab4/barrier.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 
 int count = 0;
+bool condition = true;
 
 void taskOne(){
   std::cout << "first " << count << "\n";
@@ -12,22 +13,19 @@ void taskTwo(){
   std::cout << "second " << count << "\n";
 }
 
-void barrierFunction(std::shared_ptr<Semaphore> mutex,std::shared_ptr<Semaphore> barrier1, std::shared_ptr<Semaphore> barrier2){
-
-    bool condition = true;
+void barrierFunction(std::shared_ptr <Semaphore> mutex,std::shared_ptr<Semaphore> barrier1, std::shared_ptr<Semaphore> barrier2, int arraySize){
   
     while(condition){
     mutex->Wait();
     count++;
-    if(count == threadArray.size() - 1){
+    if(count == arraySize){
       barrier2->Wait();
       barrier1->Signal();      
     }
+    taskOne();
     mutex->Signal();
     barrier1->Wait();
-    barrier1->Signal();
-
-    taskOne();
+    barrier1->Signal();  
 
     mutex->Wait();
     count--;
@@ -36,26 +34,34 @@ void barrierFunction(std::shared_ptr<Semaphore> mutex,std::shared_ptr<Semaphore>
       barrier2->Signal();
       condition = false;
     }
+    taskTwo();
     mutex->Signal();
     barrier2->Wait();
     barrier2->Signal();
-
-    taskTwo();
+    
   }
 
 
 }
 
 int main(void){
-  std::array<std::thread,10> threadArray;
+  std::thread thread1, thread2, thread3, thread4, thread5;
   std::shared_ptr<Semaphore> mutex( new Semaphore(1));
   std::shared_ptr<Semaphore> barrier1( new Semaphore(0));
   std::shared_ptr<Semaphore> barrier2( new Semaphore(1));
   /**< Launch the threads  */
 
-  for(int i = 0; i < threadArray.size();i++){
-  threadArray[i]=std::thread(barrierFunction, mutex, barrier1, barrier2);
-  }
+  thread1=std::thread(barrierFunction, mutex, barrier1, barrier2, 5);
+  thread2=std::thread(barrierFunction, mutex, barrier1, barrier2, 5);
+  thread3=std::thread(barrierFunction, mutex, barrier1, barrier2, 5);
+  thread4=std::thread(barrierFunction, mutex, barrier1, barrier2, 5);
+  thread5=std::thread(barrierFunction, mutex, barrier1, barrier2, 5);
 
+  thread1.join();
+  thread2.join();
+  thread3.join();
+  thread4.join();
+  thread5.join();
+  
   return 0;
 }
