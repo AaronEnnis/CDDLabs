50573f8ee4b32d7989bcd703d1a79aeb0b8419ca
AaronEnnis <aaronennis96@outlook.com>
2017-10-18 15:47:27 +0100

    final commit loop

--- Lab4/barrier.cpp
+++ Lab4/barrier.cpp
@@ -1,5 +1,7 @@
 #include "Semaphore.h"
 #include <iostream>
+#include <stdio.h>
+#include <cstdlib>
 #include <thread>
 #include <vector>
 
@@ -49,16 +51,48 @@ void barrierFunction(std::shared_ptr <Semaphore> mutex,std::shared_ptr<Semaphore
 
 }
 
+/*!Only allows the user to input integer */
+int getInput()
+{
+	int value = 0;
+	int inchar = 0;
+	inchar = getchar();
+	if (inchar > 47 && inchar < 58)
+	{
+		while (inchar != 10)
+		{
+			value = value * 10 + (inchar - 48);			
+			if (inchar > 57 || inchar < 48)
+			{
+				std::cout << "\nInvalid input\n";
+				value = 0;
+				break;
+			}
+			inchar = getchar();
+		}
+	}
+	else if (inchar == 10)
+	{
+		std::cout << "\nInvalid input\n";
+		value = -1;
+	}
+	else {
+		std::cout << "\nInvalid input\n";
+		value = 0;
+	}
+	return value;
+}
+
 int main(void){
 
   std::shared_ptr<Semaphore> mutex( new Semaphore(1)); /*!< mutex lock*/
   std::shared_ptr<Semaphore> barrier1( new Semaphore(0)); /*!< first barrier*/
   std::shared_ptr<Semaphore> barrier2( new Semaphore(1)); /*!< second barrier*/
 
-  int num_of_threads;
   std::cout << "input number of threads" << std::endl;
-  std::cin >> num_of_threads;
-  std::vector<std::thread> threadsArray(num_of_threads);
+  std::cout << "Type in 0 the exit\n";
+  int num_of_threads = getInput();  /*!< allows the user to set the amount of threads created*/
+  std::vector<std::thread> threadsArray(num_of_threads); /*!< array of threads */
 
   for(int i = 0; i < threadsArray.size(); i++){
   threadsArray[i]=std::thread(barrierFunction,mutex,barrier1,barrier2, threadsArray.size());
