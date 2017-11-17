75747235f618954db266cce7a79a8d063dcd800a
AaronEnnis <aaronennis96@outlook.com>
2017-11-17 18:10:54 +0000

    initial commit

---
+++ Lab5/main.cpp
@@ -0,0 +1,20 @@
+//Author: Aaron Ennis
+//Program name: barrier
+//Purpose: create a producer consumer relationship
+
+#include "Barrier.h"
+#include <thread>
+#include <vector>
+
+/*! \class Barrier
+    \brief An Implementation of a barrier Using Semaphores 
+
+   Uses C++11 features such as mutex and condition variables to implement a producer and consumer relationship
+
+*/
+
+int main(void){
+
+  
+  return 0;
+}
